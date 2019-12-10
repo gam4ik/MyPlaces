ce001c4f819769817af6d842a525e432a765b07e
Gam4ik <gambid2006@gmail.com>
2019-12-11 00:41:17 +0300

    Custom Table view cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Gamid on 11.12.2019.
+//  Copyright © 2019 Gam4ik. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    
+}
