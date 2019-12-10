790f4f9235c4d5bbd1b01100c0bb4a163e060944
Gam4ik <gambid2006@gmail.com>
2019-12-10 21:23:31 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Gamid on 10.12.2019.
+//  Copyright © 2019 Gam4ik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
