4b0d564b9499f50349662664321e1fbba7228261
Gam4ik <gambid2006@gmail.com>
2019-12-11 14:55:23 +0300

    Add new place view controller

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,31 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Gamid on 11.12.2019.
+//  Copyright © 2019 Gam4ik. All rights reserved.
+//
+
+import UIKit
+
+struct Place {
+    
+    var name: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurantImage: String?
+    
+    static let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    static func getPlaces() -> [Place] {
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: nil, restaurantImage: place))
+        }
+        
+        return places
+    }
+    
+}
